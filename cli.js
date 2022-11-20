9a21a488c146a506aea18b7ff82d31398b984454
ronjoshi-unc <ronjoshi@ad.unc.edu>
2022-11-20 16:30:48 -0500

    i think im done?

--- cli.js
+++ cli.js
@@ -21,4 +21,21 @@ const timezone = moment.tz.guess();
 const latitude = args.n || args.s * -1;
 const longitude = args.e || args.w * -1;
 
-const response = await fetch('');
+const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=' + latitude + '&longitude=' + longitude + '&daily=precipitation_hours&timezone=' + timezone);
+const data = await response.json();
+
+if (args.j){
+    console.log(data);
+    process.exit(0);
+} else {
+    const days = args.d || 1;
+    const ans = data.daily.precipitation_hours[days] > 0 ? "You might need your galoshes " : "You will not need your galoshes ";
+
+    if (days == 0) {
+    console.log(ans + "today.")
+    } else if (days > 1) {
+    console.log(ans + "in " + days + " days.")
+    } else {
+    console.log(ans + "tomorrow.")
+    }
+}
